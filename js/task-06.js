aeaf9d3578cf509d87e774b4cad00544caa77c05
Yulia Pakhachuk <yulia.pakhachuk@gmail.com>
2022-05-19 00:14:21 +0200

    add scripts 1-8 tasks

--- js/task-06.js
+++ js/task-06.js
@@ -1,5 +1,5 @@
 // Завдання 6
-// Напиши скрипт, який під час втрати фокусу на інпуті (подія blur), 
+// Напиши скрипт, який під час втрати фокусу на інпуті (подія blur),
 // перевіряє його вміст щодо правильної кількості введених символів.
 
 // <input
@@ -8,21 +8,25 @@
 //   data-length="6"
 //   placeholder="Please enter 6 symbols"
 // />
-// Яка кількість смиволів повинна бути в інпуті, 
+// Яка кількість смиволів повинна бути в інпуті,
 // зазначається в його атрибуті data - length.
 // Якщо введена правильна кількість символів,
 // то border інпуту стає зеленим, якщо неправильна кількість - червоним.
-// Для додавання стилів використовуй CSS-класи valid і invalid, 
+// Для додавання стилів використовуй CSS-класи valid і invalid,
 // які ми вже додали у вихідні файли завдання.
 
-// #validation-input {
-// border: 3px solid #bdbdbd;
-// }
 
-// #validation-input.valid {
-// border-color: #4caf50;
-// }
+const input = document.querySelector("#validation-input");
+input.addEventListener("blur", eventBlur);
 
-// #validation-input.invalid {
-// border-color: #f44336;
-// }
+function eventBlur(event) { 
+    if (event.currentTarget.value.length === Number(input.dataset.length)) {
+        input.classList.add("valid");
+        input.classList.remove("invalid")
+
+    }
+    else { 
+        input.classList.add("invalid");
+        input.classList.remove("valid")
+    }
+}
---
+++ js/task-07.js
@@ -0,0 +1,22 @@
+// Завдання 7
+// Напиши скрипт, який реагує на зміну
+// значення input#font - size - control(подія input)
+// і змінює інлайн - стиль span#text, оновлюючи властивість
+// font - size.В результаті, перетягуючи повзунок, буде змінюватися розмір тексту.
+
+// <input id="font-size-control" type="range" min="16" max="96" />
+// <br />
+// <span id="text">Abracadabra!</span>
+
+const refs = {
+    range: document.querySelector("#font-size-control"),
+    text: document.querySelector("#text")
+}
+
+console.log(refs.range);
+refs.range.addEventListener("change", fontSizeChange);
+
+function fontSizeChange() {
+    const selectedOptionValue = refs.range.value;
+    refs.text.style.fontSize = `${selectedOptionValue}px`;
+}
